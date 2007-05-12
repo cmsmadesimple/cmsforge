0fb0eededeb88ef8c73bef1591bf9e5aefec02ee
wishy <wishy@b0f54126-fe2f-0410-bd24-f7de18f6730f>
2007-05-12 00:03:58 +0000

    Some stuff for mongrel
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsforge/trunk@9 b0f54126-fe2f-0410-bd24-f7de18f6730f

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,9 +6,19 @@ role :web, "lt10.cmsmadesimple.org"
 role :db,  "lt10.cmsmadesimple.org", :primary => true
 
 set :user, "rails"
-set :deploy_to, "/var/www/cmsmadesimple.org/devtest"
+set :deploy_to, "/var/www/cmsmadesimple.org/dev"
 
 ssh_options[:paranoid] = false 
 
 set :scm_command, "/usr/bin/svn"
 #set :local_scm_command, "/opt/local/bin/svn"
+
+desc "The spinner task is used by :cold_deploy to start the application up"
+task :spinner, :roles => :app do
+  run "cd #{deploy_to}/#{current_dir} && mongrel_rails cluster::start -C #{mongrel_conf}"
+end
+
+desc "Restart the mongrel cluster"
+task :restart, :roles => :app do
+  run "cd #{deploy_to}/#{current_dir} && mongrel_rails cluster::restart -C #{mongrel_conf}"
+end
