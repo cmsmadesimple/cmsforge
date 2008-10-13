3865a4800c1f1bb9ab7d4fcecee8cda751cc5486
wishy <wishy@b0f54126-fe2f-0410-bd24-f7de18f6730f>
2008-10-13 11:16:07 +0000

    Update capistrano information for new environment Tune down the logging on production
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsforge/trunk@67 b0f54126-fe2f-0410-bd24-f7de18f6730f

--- Capfile
+++ Capfile
@@ -1,3 +1,3 @@
-require 'capistrano/version'
 load 'deploy' if respond_to?(:namespace) # cap2 differentiator
+Dir['vendor/plugins/*/recipes/*.rb'].each { |plugin| load(plugin) }
 load 'config/deploy'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,32 +1,45 @@
 set :application, "cmsforge"
-set :repository,  "http://svn.cmsmadesimple.org/svn/cmsforge/trunk"
+set :repository, "http://svn.cmsmadesimple.org/svn/cmsforge/trunk"
 
-role :app, "lt10.cmsmadesimple.org"
-role :web, "lt10.cmsmadesimple.org"
-role :db,  "lt10.cmsmadesimple.org", :primary => true
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/var/www/cmsmadesimple.org/devnew"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+# set :scm, :subversion
+set :scm, :subversion
+set :deploy_via, :remote_cache
 
 set :user, "rails"
-set :deploy_to, "/var/www/cmsmadesimple.org/dev"
-set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
+set :ssh_options, { :forward_agent => true }
+set :use_sudo, false
 
-set :scm_command, "/usr/bin/svn"
-#set :local_scm_command, "/opt/local/bin/svn"
-
-ssh_options[:paranoid] = false 
+role :web, "web2.cmsmadesimple.org"
+role :app, "web2.cmsmadesimple.org"
+role :db,  "web2.cmsmadesimple.org", :primary => true
+set :keep_releases, 3
 
 namespace :deploy do
-  desc "Starts the mongrel cluster"
-  task :start, :roles => :app do
-    run "cd #{current_path} && mongrel_rails cluster::start -C #{mongrel_conf}"
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
   end
 
-  desc "Restart the mongrel cluster"
-  task :restart, :roles => :app do
-    run "cd #{current_path} && mongrel_rails cluster::restart -C #{mongrel_conf}"
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+  
+  desc "Preserve files" 
+  task :after_update_code, :roles => [:web] do
+    run <<-CMD
+      mkdir -p -m 775 #{releases_path} #{shared_path}/system &&
+      mkdir -p -m 777 #{shared_path}/log &&
+      cp #{releases_path}/../database.yml #{release_path}/config/database.yml &&
+      cp #{releases_path}/../amazon_s3.yml #{release_path}/config/amazon_s3.yml
+    CMD
   end
 
-  desc "Stops the mongrel cluster"
-  task :stop, :roles => :app do
-    run "cd #{current_path} && mongrel_rails cluster::stop -C #{mongrel_conf}"
-  end
 end
